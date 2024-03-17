ff6abd8d27ed0821773068d1dafb9b2a393ee5fc
yamid-dev <yhrodriguez1@hotmail.com>
2024-03-17 12:10:27 -0500

    Agregué carpeta OPP en Python

---
+++ Python/OOP Python/clases_y_objetos.py
@@ -0,0 +1,16 @@
+#EstoEsPascalCase
+#estoEsCamelCase
+#esto_es_snake_case
+
+#Esto es una clase llamada Celular
+class Celular():
+    #Estos son atributos de la clase Celular
+    marca = "Samsung"
+    modelo = "S23"
+    camara = "48MP"
+
+#Estas son instancias de la clase Celular
+celular1= Celular()
+celular2= Celular()
+celular3= Celular()
+celular4= Celular()
