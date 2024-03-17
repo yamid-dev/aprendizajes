04b464f54d47199c53a4ef9bb11fd49c30bb6071
yamid-dev <yhrodriguez1@hotmail.com>
2024-03-17 12:49:50 -0500

    Método constructor (__init__)

--- Python/OOP Python/clases_y_objetos.py
+++ Python/OOP Python/clases_y_objetos.py
@@ -4,15 +4,16 @@
 
 #Esto es una clase llamada Celular
 class Celular():
-    #Estos son atributos de la clase Celular
-    marca = "Samsung"
-    modelo = "S23"
-    camara = "48MP"
+    #metodo constructor = metodo especial que cada vez que instanciamos esta clase automaticamente se ejecuta el método constructor.
+    def __init__(self,marca,modelo,camara): #self es una forma de hacer referencia a si mismo, al mismo objeto.
+        self.marca = marca #esto es como decir celular 1|2|3.marca = marca
+        self.modelo = modelo
+        self.camara = camara
+    
 
 #Estas son instancias de la clase Celular
-celular1= Celular()
-celular2= Celular()
-celular3= Celular()
-celular4= Celular()
+celular1= Celular("Samsung","S23","48MPX") #Aquí ingresamos atributos basandonos en el método constructor que nos permite reutilizar código.
 
+celular2= Celular("Apple","Iphon 15","PRO")
 print(celular1.marca)
+print(celular2.marca)
