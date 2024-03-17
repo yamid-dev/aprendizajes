e48c5e4d17f1ef56d48140355a947d125b4d733a
yamid-dev <yhrodriguez1@hotmail.com>
2024-03-17 14:48:59 -0500

    Herencia simple

--- Python/OOP Python/herencia.py
+++ Python/OOP Python/herencia.py
@@ -7,10 +7,30 @@ class Persona:
         self.nacionalidad = nacionalidad
     def hablar(self):
         print ("Hola, estoy hablando un poco...")    
-#Esta es una clase hija llamnada Empleado que hereda caracteristicas de la clase padre Persona   
-class Empleado(Persona):
-    pass
-roberto = Empleado("Roberto",43,"Colombiano")
+#Esta es una clase hija llamnada Empleado que hereda caracteristicas de la clase padre Persona   = Herencia simple
+class Empleado(Persona): #Superclase es Persona y Empleado es Subclase
+    def __init__(self,nombre,edad,nacionalidad,trabajo,salario):
+        #constructor dentro de otro constructor
+        super().__init__(nombre,edad,nacionalidad)
+        self.trabajo = trabajo
+        self.salario  = salario
+    #Aquí sobreescribí método
+    # def hablar(self):
+    #     print("Noooo")
+
+class Estudiante(Persona): #Superclase es Persona y Empleado es Subclase
+    def __init__(self,nombre,edad,nacionalidad,universidad,notas):
+        #constructor dentro de otro constructor
+        super().__init__(self,nombre,edad,nacionalidad)
+        self.universidad = universidad
+        self.notas  = notas
+    #Aquí sobreescribí método
+    # def hablar(self):
+    #     print("Noooo")
+
+roberto = Empleado("Roberto",43,"Colombiano","programador",1000000)
+
+
 
 roberto.hablar()
-print(roberto.nacionalidad)
+print(roberto.trabajo)
