4e6881b2caab79e4c85dde887e188c332a9f458a
yamid-dev <yhrodriguez1@hotmail.com>
2024-03-17 15:28:19 -0500

    Herencia multiple 2

--- Python/OOP Python/herencia.py
+++ Python/OOP Python/herencia.py
@@ -1,44 +1,46 @@
-#herencia = pilar fundamental de la OOP que permite a la clase hija acceder a todos los métodos y propiedades de todos los padres por ejemplo: persona y estudiante.
+# Herencia = pilar fundamental de la OOP que permite a la clase hija acceder a todos los métodos y propiedades de todos los padres. Por ejemplo, Persona y Estudiante.
 
 class Persona:
-    def __init__(self,nombre,edad,nacionalidad):
+    def __init__(self, nombre, edad, nacionalidad):
         self.nombre = nombre
         self.edad = edad
         self.nacionalidad = nacionalidad
+        
     def hablar(self):
-        print ("Hola, estoy hablando un poco...")   
+        print("Hola, estoy hablando un poco...")
+
 class Artista:
     def __init__(self, habilidad):
         self.habilidad = habilidad
+        
     def mostrar_habilidad(self):
         print(f"Mi habilidad es: {self.habilidad}")
-#Esta es una clase hija llamnada Empleado que hereda caracteristicas de la clase padre Persona   = Herencia simple
-class EmpleadoArtista(Persona,Artista): #Superclase es Persona y Empleado es Subclase
-    def __init__(self,nombre,edad,nacionalidad,habilidad,empresa,salario):
-        #constructor dentro de otro constructor
-        super().__init__(nombre,edad,nacionalidad)
-        self.habilidad = habilidad
-        self.salario  = salario
+
+# Esta es una clase hija llamada Empleado que hereda características de las clases padre Persona y Artista (herencia múltiple).
+class EmpleadoArtista(Persona, Artista):
+    def __init__(self, nombre, edad, nacionalidad, habilidad, salario, empresa):
+        # Constructor dentro de otro constructor
+        Persona.__init__(self, nombre, edad, nacionalidad)
+        Artista.__init__(self, habilidad)
+        self.salario = salario
         self.empresa = empresa
+        
+  
     def presentarse(self):
-        return(f'Mi habilidad es: {self.mostrar_habilidad()}')
-    #Aquí sobreescribí método
-    # def hablar(self):
-    #     print("Noooo")
+        return(f"{self.mostrar_habilidad()}")
+        
+    # Aquí se podría sobrescribir el método hablar si se desea.
 
-class Estudiante(Persona): #Superclase es Persona y Empleado es Subclase
-    def __init__(self,nombre,edad,nacionalidad,universidad,notas):
-        #constructor dentro de otro constructor
-        super().__init__(self,nombre,edad,nacionalidad)
+class Estudiante(Persona):
+    def __init__(self, nombre, edad, nacionalidad, universidad, notas):
+        # Constructor dentro de otro constructor
+        super().__init__(nombre, edad, nacionalidad)
         self.universidad = universidad
-        self.notas  = notas
-    #Aquí sobreescribí método
-    # def hablar(self):
-    #     print("Noooo")
+        self.notas = notas
 
-roberto = EmpleadoArtista("Roberto",43,"Colombiano","Cantar","programador",1000000)
+    # Aquí se podría sobrescribir el método hablar si se desea.
 
-
+roberto = EmpleadoArtista("Roberto", 43, "Colombiano", "Cantar",1000000,"MARVY")
 
 roberto.hablar()
-roberto.mostrar_habilidad()
+roberto.presentarse()
