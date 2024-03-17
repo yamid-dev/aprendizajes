f934cad2bc65cbdd5f569bb2c8fc091bb0ec7127
yamid-dev <yhrodriguez1@hotmail.com>
2024-03-17 16:29:13 -0500

    MRO metodo de resolucion de orden 6

--- Python/OOP Python/mro.py
+++ Python/OOP Python/mro.py
@@ -1,17 +1,19 @@
 #mro = metodo de resolución de orden.  Determinar el orden en el que buscará los métodos y atributos de esas clases base cuando se llame a un método o se acceda a un atributo desde una instancia de esa clase derivada.
 
-class A:
+class A():
     def hablar(self):
         print("Hablando desde A")
 
 class B(A):
+    def hablar(self):
+        print("Hablando desde B")
+
+class C(B,A):
     pass
 
-class C(A):
-    pass
-
-class D(B,C):
+class D(C):
     pass
 #D>B>C>A
 d=D()
 d.hablar()
+print(D.__mro__)
