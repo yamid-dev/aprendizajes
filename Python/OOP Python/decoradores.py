44a909ba061fed38bffeedadbf4498703bd17997
yamid-dev <yhrodriguez1@hotmail.com>
2024-03-18 10:54:22 -0500

    Explicacion decoradores de clases, args y kwargs

--- Python/OOP Python/decoradores.py
+++ Python/OOP Python/decoradores.py
@@ -1,25 +1,33 @@
 #Decoradores  = función especial que decora a otra. El decorador no agarra la función y la modifica, sino que le agrega funcionalidad antes y/o después de ejecutarla.
 
-def decordador(function):
-    def function_modificada():
-        print("Antes de llamar a la función")
-        function()
-        print("Después de llamar a la función")
-    return function_modificada
-
-# def saludo():
-#     print("Hola Yamid")
-    
-# saludo_modificado = decordador(saludo)
-
-# saludo_modificado()
-
-@decordador
-def saludo():
-    print("Hola Yamid")
-
-saludo()
-
 #Decoradores de clases: Son funciones que toman una clase como argumento y devuelven una clase modificada o extendida de alguna manera. Se utlizan para modificar el comportamiento de una clase sin camvbiar su implementación directamente. Proporciona una forma elegante y reutlizable de agregar funcionalidades adicionales a las  clases
+def decorador_clase(cls):
+    class NuevaClase:
+        #*args es un parametro especial que se utiliza en la definición de funciones para indicar que la función puede aceptar un número variable de argumentos posicionales. Los argumentos pasados se recopilan en nuestra tupla dentro de la función
+        
+        #**kwargs es un parámetro especial que se utiliza en la definición de funciones para indicar que la función puede aceptar un número variable de argumentos de palabra clave (con nombres). Los argumentos pasados se recopilan en un diccionario dentro de la función
+        
+        def __init__(self,*args,**kwargs):
+            self.objeto_original = cls(*args,**kwargs)
+            self.atributo_nuevo = "Atributo añadido por el decorador"
+        
+        #getarr es una función integrada de Python que se utiliza para obtener el valor de un atributo de un objeto dado su nombre. Toma tres argumentos: objeto del que se quiere obtener el atributo, el nombre del atributo y opcionalmente un valor por defecto que se volverá si el atributo no existe
+        def __getattr__(self,nombre):
+            return getattr(self.objeto_original,nombre)
+    return NuevaClase
 
+@decorador_clase
+class MiClase:
+    def __init__(self,valor):
+        self.valor = valor
+    
+    def metodo(self):
+        print("Método original")
+        
+objeto = MiClase(10)
+
+print(objeto.valor)
+objeto.metodo()
+
+print(objeto.atributo_nuevo)
 #Decoradores multiples: Capacidad de aplicar más de un decorador a una función o método. Esto permite modulizar y organizar el código de manera más eficiente, ya que te permite aplicar múltiples transformaciones o funcionalidades a una función o método de forma transparente y ordenada.
