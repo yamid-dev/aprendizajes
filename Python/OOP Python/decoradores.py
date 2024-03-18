e64cf319b7389119fba49ce36d556b1a7e5d3769
yamid-dev <yhrodriguez1@hotmail.com>
2024-03-18 09:09:30 -0500

    Decoradores multiples y de clases definicion

--- Python/OOP Python/decoradores.py
+++ Python/OOP Python/decoradores.py
@@ -1,4 +1,4 @@
-#Decoradores  = función especial que decora a otra
+#Decoradores  = función especial que decora a otra. El decorador no agarra la función y la modifica, sino que le agrega funcionalidad antes y/o después de ejecutarla.
 
 def decordador(function):
     def function_modificada():
@@ -19,3 +19,7 @@ def saludo():
     print("Hola Yamid")
 
 saludo()
+
+#Decoradores de clases: Son funciones que toman una clase como argumento y devuelven una clase modificada o extendida de alguna manera. Se utlizan para modificar el comportamiento de una clase sin camvbiar su implementación directamente. Proporciona una forma elegante y reutlizable de agregar funcionalidades adicionales a las  clases
+
+#Decoradores multiples: Capacidad de aplicar más de un decorador a una función o método. Esto permite modulizar y organizar el código de manera más eficiente, ya que te permite aplicar múltiples transformaciones o funcionalidades a una función o método de forma transparente y ordenada.
