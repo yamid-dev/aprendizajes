f1ba81ec45b04f8227ca066397fc30ed70c04f8d
yamid-dev <yhrodriguez1@hotmail.com>
2024-03-18 08:16:17 -0500

    Decoradores

---
+++ Python/OOP Python/decoradores.py
@@ -0,0 +1,15 @@
+#Decoradores  = función especial que decora a otra
+
+def decordador(function):
+    def function_modificada():
+        print("Antes de llamar a la función")
+        function()
+        print("Después de llamar a la función")
+    return function_modificada
+
+def saludo():
+    print("Hola Yamid")
+    
+saludo_modificado = decordador(saludo)
+
+saludo_modificado()
--- Python/OOP Python/encapsulamiento.py
+++ Python/OOP Python/encapsulamiento.py
@@ -2,22 +2,22 @@
 #Getters y Setters: Getters obtenedor y setters establecedor. Dos métodos para acceder a propiedades privadas que tienen las clases o incluso modificarlas.
 class Persona:
     def __init__(self,nombre,edad):
-        self.__nombre = nombre
-        self.__edad = edad
+        self._nombre = nombre
+        self._edad = edad
    
     #Getter es un método que nos permite acceder a un valor "privado" o "muy privado"
     def get_nombre(self):
-        return self.__nombre
+        return self._nombre
     
     #Setter es un método que permite establecer un valor "privado" o "muy privado"
     def set_nombre(self, new_nombre):
-        self.__nombre = new_nombre
+        self._nombre = new_nombre
 
 #En este bloque instancié un objeto de la clase Persona, después accedí a un atributo privado nombre con el getter, lo imprimí, establecí un nuevo nombre con setter, después volví a acceder al mismo con getter para finalmente volverlo a imprimir
 persona = Persona("Yamid",21)
 nombre = persona.get_nombre()
 print(nombre)
-persona.__nombre = "Pepito"
+persona.set_nombre("Pepito")
 nombre = persona.get_nombre()
 print(nombre)
 
