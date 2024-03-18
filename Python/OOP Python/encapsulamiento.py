858f4a548c902e66dec0d804fb3ebe5b97b0e7da
yamid-dev <yhrodriguez1@hotmail.com>
2024-03-18 08:02:15 -0500

    Encapsulamiento: getters y setters para atributos muy privados

--- Python/OOP Python/encapsulamiento.py
+++ Python/OOP Python/encapsulamiento.py
@@ -1,18 +1,17 @@
 
 #Getters y Setters: Getters obtenedor y setters establecedor. Dos métodos para acceder a propiedades privadas que tienen las clases o incluso modificarlas.
-
 class Persona:
     def __init__(self,nombre,edad):
-        self._nombre = nombre
-        self._edad = edad
+        self.__nombre = nombre
+        self.__edad = edad
    
     #Getter es un método que nos permite acceder a un valor "privado" o "muy privado"
     def get_nombre(self):
-        return self._nombre
+        return self.__nombre
     
     #Setter es un método que permite establecer un valor "privado" o "muy privado"
     def set_nombre(self, new_nombre):
-        self._nombre = new_nombre
+        self.__nombre = new_nombre
 
 #En este bloque instancié un objeto de la clase Persona, después accedí a un atributo privado nombre con el getter, lo imprimí, establecí un nuevo nombre con setter, después volví a acceder al mismo con getter para finalmente volverlo a imprimir
 persona = Persona("Yamid",21)
