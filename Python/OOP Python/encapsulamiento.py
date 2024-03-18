1d7012c5c0566457065598fbb425dafdf65773be
yamid-dev <yhrodriguez1@hotmail.com>
2024-03-18 07:38:36 -0500

    Encapsulamiento: metodo muy privado

--- Python/OOP Python/encapsulamiento.py
+++ Python/OOP Python/encapsulamiento.py
@@ -2,8 +2,13 @@ class MiClase:
     def __init__(self):
         #Atributo muy privado
         self.__atributo_privado = "Valor"
+    #Método muy privado
+    def __hablar(self):
+        print("Hola, estoy hablando")
         
 objeto = MiClase()
-print(objeto.__atributo_privado)
+print(objeto.__hablar()) 
 
 #El atributo privado de python es el protedigo de otros lenguajes y el atributo mu privado es el privado de otros lenguajes
+
+#Getters y Setters: Getters obtenedor y setters establecedor. 
