731e98c3d00305f723c9b4c80c6e83785dd630e4
yamid-dev <yhrodriguez1@hotmail.com>
2024-03-19 07:15:59 -0500

    Ejemplo de error cuando quieres cambiar una propiedad sin setter

--- Python/OOP Python/decoradores_property.py
+++ Python/OOP Python/decoradores_property.py
@@ -6,13 +6,13 @@ class Persona:
     
     #property es un decorador especial porque es un decorador reservado para las clases, que le dice esto es un getter, usalo como una propiedad
     @property
-    def get_nombre(self):
+    def nombre(self):
         return self.__nombre
 
 
 persona = Persona("Yamid",21)
 
 #property le dice no trates el getter como una función sino como una propiedad, compara con el archivo de encapsulamiento
-nombre = persona.get_nombre
-
+nombre = persona.nombre
+persona.nombre = "Horacio"
 print(nombre)
