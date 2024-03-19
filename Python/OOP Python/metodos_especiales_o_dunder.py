f520bda2c05eed295e4b548230bc38ae3e762b87
yamid-dev <yhrodriguez1@hotmail.com>
2024-03-19 09:08:50 -0500

    Metodo especial __repr__ y explicación de eval()

--- Python/OOP Python/metodos_especiales_o_dunder.py
+++ Python/OOP Python/metodos_especiales_o_dunder.py
@@ -7,6 +7,15 @@ class Persona:
     #el método especial str que devuelve una representacion del objeto en una cadena de texto
     def __str__(self):
         return f'Persona(nombre={self.nombre}, edad={self.edad})'
+    
+    def __repr__(self):
+        return f'Persona("{self.nombre}",{self.edad})'
 
 persona = Persona("Yamid",21)
-print(persona)
+repre = repr(persona) #repre es la representación del objeto para luego reconstruirlo
+resultado = eval(repre) #resultado ya es el objeto reconstruido, eval se utiliza para evaluar una cadena de texto que representa una expresión o declaración en Python. En este caso repre contiene una representación en forma de cadena del objeto Persona generada utilizando __repr__.
+
+#Cuando se llama eval(repre) se evalua la cadena repre, que contiene la representación de la instancia de la clase Persona. Esto tiene en el efecto de reconstruir un nuevo objeto Persona utilizando la cadena de representación. En esencia, 'eval' ejecuta el código representado por la cadena. Se debe utilizar con precaución especialmente en aplicaciones que interactuan con usuarios externos o datos no confiables ya que ejecuta el código de la cadena de manera directa y puede ser una puerta trasera pra inyección de código malicioso.
+
+print(repre)
+print(resultado.nombre)
