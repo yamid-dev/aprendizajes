43442c2326967a54ded8d8ad2e0d45ddf0b1ea25
yamid-dev <yhrodriguez1@hotmail.com>
2024-03-19 10:39:40 -0500

    Sobrecarga de operadores

--- Python/OOP Python/metodos_especiales_o_dunder.py
+++ Python/OOP Python/metodos_especiales_o_dunder.py
@@ -19,3 +19,37 @@ resultado = eval(repre) #resultado ya es el objeto reconstruido, eval se utiliza
 
 print(repre)
 print(resultado.nombre)
+
+#Sobrecarga de operadores = ó operator overloading. Es una caracteristica que permite definir cómo deben comportarse los operadores cuando se aplica a tipos de datos personalizados, como clases y objetos. Permite proporcionar una implementación personalizada para operadores estándar como +,-,*,/,%, ==, !=, etc. en tus propias clases. Puedes definir el significado de estos operadores para tus propios tipos de datos, lo que les permite comportarse de la misma manera que los tipos de datos incorporados por el lenguaje.
+
+'''
+Otros métodos especiales:
+-------Aritméticos-------
+__add__(self,other): Sobrecarga de operador de suma (+)
+__sub__(self,other): Sobrecarga de operador de resta (-)
+__mul__(self,other): Sobrecarga de operador de multiplicación (*)
+__div__(self,other): Sobrecarga de operador de división (/)
+__mod__(self,other): Sobrecarga de operador de módulo (%)
+__pow__(self,other): Sobrecarga de operador de exponenciación (**)
+
+-------Comparación---------
+__eq__(self,other): Sobrecarga de operador de igualdad (==)
+__ne__(self,other): Sobrecarga de operador de desigualdad (!=)
+__lt__(self,other): Sobrecarga de operador de menor que (<)
+__gt__(self,other): Sobrecarga de operador de mayor que (>)
+__le__(self,other): Sobrecarga de operador de menor o igual que (<=)
+__ge__(self,other): Sobrecarga de operador de mayor o igual que (>=)
+
+-------Asignación---------
+__iadd__(self,other): Sobrecarga de operador de suma en asignación (+=)
+__isub__(self,other): Sobrecarga de operador de resta en asignación (-=)
+__imult__(self,other): Sobrecarga de operador de multiplicación en asignación (*=)
+__idiv__(self,other): Sobrecarga de operador de división en asignación (/=)
+__imod__(self,other): Sobrecarga de operador de módulo en asignación (%=)
+__ipow__(self,other): Sobrecarga de operador de exponenciación en asignación (**=)
+
+-------Otros---------
+__str__(self): Sobrecarga de operador str(). Devuelve una representación legible como cadena del objeto
+__len__(self): Sobrecarga de operador len(). Devuelve la longitud del objeto
+__getitem__(self,index): Sobrecarga del operador de indexación([]). Permite acceder a elementos del objeto por indice
+'''
