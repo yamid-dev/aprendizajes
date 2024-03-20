6c8cdcd31377778e7394ff1c0a36d46a9abc35db
yamid-dev <yhrodriguez1@hotmail.com>
2024-03-20 16:47:45 -0500

    Principio de sustitucion de Liskov 1

--- Python/OOP Python/solid.py
+++ Python/OOP Python/solid.py
@@ -1,27 +1,15 @@
 #SRC,OCP,LCP,ISP,DIP
-#PRINCIPIO ABIERTO/CERRADO (OCP) = LAS CLASES, LOS MODULOS, LAS FUNCIONES TIENEN QUE ESTAR ABIERTAS PARA LA EXTENSION PERO CERRADAS PARA LA MODIFICACIÓN. NO ES BUENO MODIFICAR EL CÓDIGO DE UNA CLASE
+#PRINCIPIO DE SUSTITUCIÓN DE LISKOV (LSP) =  Las clases derivadas deben ser sustituidas por sus clases base
 
-class Notificador:
-    def __init__(self, usuario, mensaje):
-        self.usuario = usuario
-        self.mensaje = mensaje
-
-    def notificar(self):
-        raise NotImplementedError
+#ESTO NO CUMPLE EL PRINCIPIO DE SUSTITUCIÓN DE LISKOV PORQUE EL PINGUINO DEBERÍA VOLAR SI HEREDA DE LA CLASE AVE.
+class Ave:
+    def volar(self):
+        return "Estoy volando"
+class Pinguino(Ave):
+    def volar(self):
+        return "No puedo volar"
     
-class Notificador_Email(Notificador):
-    def notificar (self):
-        print(f"Enviando mensaje a {self.usuario.email}")
+def hacer_volar(ave = Ave):
+    return ave.volar()
 
-class Notificador_SMS(Notificador):
-    def notificar(self):
-        print(f"Enviando mensaje a {self.usuario.sms}")
-
-class Notificador_Whatsapp(Notificador):
-    def notificar(self):
-        print(f"Enviando mensaje a {self.usuario.whatsapp }")
-
-#Puedo agregar más implementaciones de la clase Notificador, pero no estoy modificando la misma cumpliendo de esta manera el principio de abierto/cerrado
-class Notificador_Twitter(Notificador):
-    def notificar(self):
-        print(f"Enviando mensaje a {self.usuario.twitter}")
+print(hacer_volar(Pinguino()))
