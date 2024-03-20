e7e0270ab5bbe4c2a5d61cd603719eac8d93ea83
yamid-dev <yhrodriguez1@hotmail.com>
2024-03-20 15:51:43 -0500

    Explicación SOLID

---
+++ Python/OOP Python/solid.py
@@ -0,0 +1,18 @@
+'''Principios SOLID = 
+
+¿Hasta dónde voy a llevar lo que estoy escribiendo?
+
+¿Nos importa que crezca?
+
+Si queremos que crezca debemos hacer que sea mantenible y modificable, tener un estructura clara, que sea reutilizable (modulos,clases y funciones), independientes, y que se puedan utilizar en otro proyecto. Que sea facil y entendible de leer, comentar cuando sea necesario, convenciones de estilo, que sea extensible sin necesidad de afectar el código.
+
+S->SRP = PRINCIPIO DE RESPONSABILIDAD ÚNICO
+O->OCP = PRINCIPIO DE ABIERTO/CERRADO
+L->LCP = PRINCIPIO DE SUSTITUCIÓN DE LOSKOV
+I->ISP = PRINCIPIO DE SEGREGACIÓN DE INTERFAZ
+D->DIP = PRINCIPIO DE INVERSIÓN DE DEPENDENCIAS 
+
+NO SEGUIR LOS PRINCIPIOS SOLID ES UNA MALA PRÁCTICA
+
+
+'''
