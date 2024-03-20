548699ea7e7c2500b4d7f4482d4f707fe7f2a48b
yamid-dev <yhrodriguez1@hotmail.com>
2024-03-20 16:51:37 -0500

    Principio de sustitucion de Liskov 2

--- Python/OOP Python/solid.py
+++ Python/OOP Python/solid.py
@@ -2,14 +2,25 @@
 #PRINCIPIO DE SUSTITUCIÓN DE LISKOV (LSP) =  Las clases derivadas deben ser sustituidas por sus clases base
 
 #ESTO NO CUMPLE EL PRINCIPIO DE SUSTITUCIÓN DE LISKOV PORQUE EL PINGUINO DEBERÍA VOLAR SI HEREDA DE LA CLASE AVE.
+# class Ave:
+#     def volar(self):
+#         return "Estoy volando"
+# class Pinguino(Ave):
+#     def volar(self):
+#         return "No puedo volar"
+    
+# def hacer_volar(ave = Ave):
+#     return ave.volar()
+
+# print(hacer_volar(Pinguino()))
+
+#ESTO SERÍA APLICAR EL PRINCIPIO DE SUSTITUCIÓN DE LISKOV
 class Ave:
+    pass
+
+class AveVoladora(Ave):
     def volar(self):
         return "Estoy volando"
-class Pinguino(Ave):
-    def volar(self):
-        return "No puedo volar"
-    
-def hacer_volar(ave = Ave):
-    return ave.volar()
 
-print(hacer_volar(Pinguino()))
+class AveNoVoladora(Ave):
+    pass
