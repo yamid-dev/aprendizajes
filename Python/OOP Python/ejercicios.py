7db09c86f248f03127837cdb264d6721978dc0a4
yamid-dev <yhrodriguez1@hotmail.com>
2024-03-20 15:21:07 -0500

    Ejercicio 3: juego de fusión

--- Python/OOP Python/ejercicios.py
+++ Python/OOP Python/ejercicios.py
@@ -6,12 +6,19 @@ Para ello deberemos cambiar el comportamiento del operador "+" para que cuando l
 Una posible formula es: el promedio de las habilidades de ambos al cuadrado'''
 
 class Personaje:
-    def __init___(self, nombre, fuerza, velocidad):
+    def __init__(self, nombre, fuerza, velocidad):
         self.nombre = nombre
         self.fuerza = fuerza
         self.velocidad = velocidad
     def __repr__(self):
         return f"{self.nombre} (fuerza: {self.fuerza}, velocidad: {self.velocidad})"
+    def __add__(self,otro_pj):
+        nuevo_nombre = self.nombre+"+"+otro_pj.nombre
+        nueva_fuerza = ((self.fuerza + otro_pj.fuerza)/2)**2
+        nueva_velocidad = ((self.velocidad + otro_pj.velocidad)/2)**2
+        return Personaje(nuevo_nombre,nueva_fuerza,nueva_velocidad)
     
 goku = Personaje("Goku",100,100)
-print(goku)
+vegeta= Personaje("Vegeta",99,99)
+gogeta=goku+vegeta
+print(gogeta)
