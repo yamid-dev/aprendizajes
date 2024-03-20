dcd66a1378cbd3671b591a4f6f8d06369a6327c6
yamid-dev <yhrodriguez1@hotmail.com>
2024-03-20 16:15:42 -0500

    SRC: Principio de responsabilidad unica 2

--- Python/OOP Python/solid.py
+++ Python/OOP Python/solid.py
@@ -2,19 +2,42 @@
 #PRINCIPIO DE RESPONSABILIDAD ÚNICA (SRC) =  Una clase solo tiene que tener una responsabilidad o tarea, tiene que tener una y solo una razón para cambiar.
 
 
-#ESTO NO SIGUE EL PRINCIPIO DE RESPONSABILIDAD ÚNICA
+#ESTO SI SIGUE EL PRINCIPIO DE RESPONSABILIDAD ÚNICA SRC.
 class Auto():
-    def __init__(self):
+    def __init__(self,tanque):
         self.posicion = 0
-        self.combustible = 100
+        self.tanque = tanque
     def mover(self,distancia):
-        if self.comustible >= distancia/2:
+        if self.tanque.obtener_combustible() >= distancia/2:
             self.posicion +=distancia
-            self.combustible -= distancia/2
+            self.tanque.usar_combustible(distancia/2)
+            return "El auto se está moviendo"
         else:
-            print("No hay suficiente combustible")
+            return "No hay sufisciente combustible"
+    def obtener_posicion(self):
+        return self.posicion
+
+class TanqueDeCombustible:
+    def __init__(self):
+        self.combustible = 100
+
     def agregar_combustible(self,cantidad):
         self.combustible +=cantidad
     
     def obtener_combustible(self):
         return self.combustible
+    def usar_combustible(self,cantidad):
+        self.combustible -= cantidad
+tanque = TanqueDeCombustible()
+autito = Auto(tanque)
+print(autito.obtener_posicion())
+print(autito.mover(10))
+print(autito.obtener_posicion())
+print(autito.mover(20))
+print(autito.obtener_posicion())
+print(autito.mover(60))
+print(autito.obtener_posicion())
+print(autito.mover(100))
+print(autito.obtener_posicion())
+print(autito.mover(100))
+print(autito.obtener_posicion())
