8619d144b385ea69ebe3dd50932db5c3ad1dfd09
yamid-dev <yhrodriguez1@hotmail.com>
2024-03-20 20:45:24 -0500

    Ejercicio 4: analizador de sentimientos 2

--- Python/OOP Python/ejercicios.py
+++ Python/OOP Python/ejercicios.py
@@ -1,32 +1,46 @@
 #Ejercicio 4 = Chatbot en Python que nos pida que le digamos algo, tome eso que le decimos, analice sentimiento, y responda cuál es el sentimiento
 
-#Este código no cumple los principios SOLID
+#Este código si cumple los principios SOLID
 import openai
 
-openai.api_key = "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"
+openai.api_key = "sk-"
 
 system_rol = "Haz de cuenta que eres un analizador de sentimiento. Yo te doy sentimientos y tu analizas el sentimiento de los mensajes y das una respuesta con al menos 1 caracter y como máximo 4 caracteres. SOLO RESPUESTAS NUMÉRICAS. Donde 1 es negatividad máxima, 0 es neutral y 1 es positividad máxima (puedes responder con ints o floats)"
 
 mensajes = [{"role":"system",'content':system_rol}]
 
-class AnalizadorDeSentimientos:
-    def analizar_sentimiento(self, polaridad):
-        if polaridad > -0.8 and polaridad <= -0.3:
-            return "1"  # Muy negativo
-        elif polaridad > -0.3 and polaridad < -0.1:
-            return "2"  # Algo negativo
-        elif polaridad > -0.1 and polaridad <= 0.1:
-            return "0"  # Neutral
-        elif polaridad > 0.1 and polaridad <= 0.4:
-            return "3"  # Algo positivo
-        elif polaridad > 0.4 and polaridad <= 0.9:
-            return "4"  # Positivo
-        elif polaridad > 0.9:
-            return "5"  # Muy positivo
-        else:
-            return "1"  # Muy negativo
+#Creamos la clase sentimiento que tiene la responsabilidad única de representar un sentimiento y su color
+class Sentimientos:
+    def __init__(self,nombre,color):
+        self.nombre = nombre
+        self.color = color
+    def __str__(self):
+        return "\x1b[1;{}m{}\x1b[0;37m".format(self.color,self.nombre)
 
-analizador = AnalizadorDeSentimientos()
+#Creamos la clase AnalizadorDeSentimientos que tiene la responsabilidad de mapear polaridades a sentimiento, se encarga de definirnos cuál será en sentimiento
+    
+#AnalizadorDeSentimiento depende de Sentimientos Y de Rangos, y no dependemos de una biblioteca externa, sino dependemos de las implementaciones de los métodos de la clase Sentimientos y de los rangos.
+class AnalizadorDeSentimientos:
+    def __init__(self,rangos):
+        self.rangos = rangos
+    def analizar_sentimiento(self,polaridad):
+        for rango, sentimiento in self.rangos:
+            if rango[0] < polaridad <= rango[1]:
+                return sentimiento
+        #si no está en el rango es muy negativo
+        return Sentimientos("Muy Negativo","31")
+    
+#Cumplimos con el principio abierto/cerrado pues está abierto para extensiones ya que podemos agregar más rangos sin necesidad de modificar el código
+rangos = [
+    (-0.6,-0.3),Sentimientos("negativo","31"),
+    (-0.3,-0.1),Sentimientos("algo negativo","31"),
+    (-0.1,0.1),Sentimientos("neutral","31"),
+    (0.1,0.4),Sentimientos("algo positivo","31"),
+    (0.4,0.9),Sentimientos("positivo","31"),
+    (0.9,1.0),Sentimientos("muy positivo","31"),
+]
+
+analizador = AnalizadorDeSentimientos(rangos)
 
 while True:
     user_prompt = input("\x1b[1;33m"+" \nDime algo: " + "\x1b[0;37m")
