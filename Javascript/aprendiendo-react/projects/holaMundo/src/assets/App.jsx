354498f6bc40f32d226435d023e4fa747f165c05
yamid-dev <yhrodriguez1@hotmail.com>
2024-04-22 10:18:29 -0500

    proyecto twitter cards componentes vs elementos

--- Javascript/aprendiendo-react/projects/holaMundo/src/assets/App.jsx
+++ Javascript/aprendiendo-react/projects/holaMundo/src/assets/App.jsx
@@ -2,12 +2,30 @@ import React from 'react'
 import './App.css'
 import { TwitterFollowCard } from './TwitterFolloCard'
 export function App () {
+    //Esto es un elemento
+    const formattedUserName = <span>@YamidDev</span>
+    //esto es un componente reutilizable 
     return(
-        <>
-            <TwitterFollowCard userName="yamiddev" name="Yamid Horacio Rodríguez"/>
-            <TwitterFollowCard userName="midudev" name="Mauricio Rozo" />
-            <TwitterFollowCard userName="christian" name="Christian Martinez"/>
-            <TwitterFollowCard userName="valexd" name="Valentina Velandia"/>
-        </>
+        <section className='App'>
+            <TwitterFollowCard 
+            formattedUserName = {formattedUserName}
+            userName="yamiddev" 
+            name="Yamid Horacio Rodríguez"/>
+
+            <TwitterFollowCard
+            formattedUserName = {formattedUserName} 
+            userName="midudev" 
+            name="Mauricio Rozo" />
+
+            <TwitterFollowCard
+            formattedUserName = {formattedUserName}
+            userName="christian" 
+            name="Christian Martinez"/>
+
+            <TwitterFollowCard
+            formattedUserName = {formattedUserName} 
+            userName="valexd" 
+            name="Valentina Velandia"/>
+        </section>
     )
 }
--- Javascript/aprendiendo-react/projects/holaMundo/src/assets/TwitterFolloCard.jsx
+++ Javascript/aprendiendo-react/projects/holaMundo/src/assets/TwitterFolloCard.jsx
@@ -1,11 +1,11 @@
-export function TwitterFollowCard ({userName,name}){
+export function TwitterFollowCard ({formattedUserName,userName,name}){
     return(
         <article className='tw-followCard'>
             <header className='tw-followCard-header'>
                 <img className='tw-followCard-avatar' src="https://scontent.fbog4-1.fna.fbcdn.net/v/t39.30808-6/370379864_1036709290857076_2337778916240228598_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFUV0OAeXCc3ozipSnYEsZF6knXuZE33fPqSde5kTfd84ESUCN3IXMKWnVhh-sTuP-4eswsqZwZJmkliPP1oJWE&_nc_ohc=15xnB80K4E8Ab6U-mF7&_nc_ht=scontent.fbog4-1.fna&oh=00_AfB3tb_7_nensuZo4cFu11ljDXPU122tKn4XqfiuAgCJYA&oe=662BDC4B" alt="imagen" />
                 <div className='tw-followCard-info'>
                     <strong>{name}</strong>
-                    <span className='tw-followCard-infoUserName'>@{userName}</span>
+                    <span className='tw-followCard-infoUserName'>{formattedUserName}</span>
                 </div>
             </header>
             < aside>
